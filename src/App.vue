33eb179c892c00d05a3108f8d85b856464e69391
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2022-12-10 11:44:00 +0100

    Basic communication between components example

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+import Card from './components/Card.vue'
+let showCard = ref(false);
+</script>
+
+<template>
+  <div>
+    <Card :renderCard="showCard" @closeCard="showCard = false" />
+    <button type="button" @click="showCard = true">Open card</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="renderCard">
+        <h1>Título</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, sunt! Tempore unde harum repellendus nisi
+            provident fuga id quia veniam natus at nulla, dolore sunt nam odio, earum ullam ut!</p>
+        <button type="button" @click="closeCard">Close card</button>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps(['renderCard']);
+const eventEmitter = defineEmits(['closeCard'])
+
+function closeCard() {
+    eventEmitter('closeCard')
+}
+</script>
+
+<style>
+
+</style>
